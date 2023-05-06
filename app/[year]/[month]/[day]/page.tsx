613b26fcbf01f5bf8860d79fc33fe0b02faab4d7
Tristan Dubbeld <tristan.dubbeld@gmail.com>
2023-05-06 10:23:25 +0200

    Add date-fns and format day date

--- app/[year]/[month]/[day]/page.tsx
+++ app/[year]/[month]/[day]/page.tsx
@@ -1,15 +1,24 @@
+import formatDate from "date-fns/format";
+
 export default function DayPage({
   params,
 }: {
-  params: { year: string; month: string; day: string }
+  params: { year: number; month: number; day: number };
 }) {
+  const monthIndex = params.month - 1; // Date months are zero based
+  const date = new Date(params.year, monthIndex, params.day);
+  const dayName = formatDate(date, "EEEE");
+  const dateName = formatDate(date, "do MMMM yyyy");
+
   return (
     <div>
       <h1>
         Day {params.day} {params.month} {params.year}
       </h1>
+      <div>{dayName}</div>
+      <div>{dateName}</div>
     </div>
-  )
+  );
 }
 
 // TODO: only allow days that exist in selected month
